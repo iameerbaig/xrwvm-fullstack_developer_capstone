e785489c501206ad556c536d28a7f3e81ba6198e
“@iameerbaig” <iameerbaig@gmail.com>
2026-07-28 03:32:49 -0400

    Fix lint errors; exclude generated dirs from lint

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -33,7 +33,6 @@ class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
     country = models.CharField(max_length=100, blank=True)
-   # dealer_id = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.name
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,12 +1,7 @@
 # Uncomment the required imports before adding the code
 
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -56,7 +51,7 @@ def logout_request(request):
 # ...
 @csrf_exempt
 def registration(request):
-    context = {}
+    pass
 
     # Load JSON data from the request body
     data = json.loads(request.body)
@@ -66,7 +61,6 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
@@ -144,10 +138,10 @@ def get_dealer_details(request, dealer_id):
 # def add_review(request):
 # ...
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
         except BaseException:
             return JsonResponse({"status": 401, "message": "Error in posting review"})
